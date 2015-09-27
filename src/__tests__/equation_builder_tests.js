fd8d3685223e36d9624cef440de70f0f68b2bf5a
Thom Wright <github@thomwright.co.uk>
2015-09-27 21:58:21 +0100

    Change API - use simple, single functions instead of complex literate API. Fix #2

--- src/__tests__/equation_builder_tests.js
+++ src/__tests__/equation_builder_tests.js
@@ -11,7 +11,7 @@ const blankThreeNodeEquation = {
 };
 const arraysForBlankThreeNode = [blankThreeNodeEquation.nodalAdmittances(), blankThreeNodeEquation.inputs()];
 
-describe('Equation Builder:', function() {
+describe('Equation Builder initialisation', function() {
   it('should initialise to a blank equation', function() {
     const {getEquation} = createEquationBuilder({
             numOfNodes: 3
@@ -20,6 +20,7 @@ describe('Equation Builder:', function() {
           arrays = [nodalAdmittances(), inputs()];
     expect(arrays).to.eql(arraysForBlankThreeNode);
   });
+
   it('should accept number of voltage sources as an optional parameter', function() {
     const {getEquation} = createEquationBuilder({
             numOfNodes: 2,
@@ -29,6 +30,7 @@ describe('Equation Builder:', function() {
           arrays = [nodalAdmittances(), inputs()];
     expect(arrays).to.eql(arraysForBlankThreeNode);
   });
+
   it('should throw an exception if given a number of nodes < 2', function() {
     expect(() => {
       createEquationBuilder({
@@ -36,6 +38,7 @@ describe('Equation Builder:', function() {
       });
     }).to.throw(/.*Number of nodes.*/);
   });
+
   it('should throw an exception if given a number of voltage sources < 0', function() {
     expect(() => {
       createEquationBuilder({
@@ -47,138 +50,109 @@ describe('Equation Builder:', function() {
 });
 
 describe('Stamping:', function() {
-  it('should not accept out of bounds nodes', function() {
-    const {stamp} = createEquationBuilder({
-            numOfNodes: 3
-          }),
-          between = stamp(10).ohms.between;
-    expect(() => { between(0, 3); }).to.throw(/.*to node.*/);
-    expect(() => { between(-1, 2); }).to.throw(/.*from node.*/);
-  });
   describe('stamping a resistance', function() {
     it('should stamp a resistance into the nodal admittance matrix', function() {
-      const {stamp, getEquation} = createEquationBuilder({
-        numOfNodes: 3
-      });
-      stamp(5).ohms.between(1, 2);
-      expect(getEquation().nodalAdmittances()).to.eql([[1 / 5, -1 / 5],
-                                                       [-1 / 5, 1 / 5]]);
-      expect(getEquation().inputs()).to.eql(blankThreeNodeEquation.inputs());
+      const {stampResistor, getEquation} = createEquationBuilder({ numOfNodes: 3 });
+      stampResistor(5, 1, 2);
+      const {nodalAdmittances, inputs} = getEquation();
+      expect(nodalAdmittances()).to.eql([[1 / 5, -1 / 5],
+                                         [-1 / 5, 1 / 5]]);
+      expect(inputs()).to.eql(blankThreeNodeEquation.inputs());
     });
+
     it('should be additive', function() {
-      const {stamp, getEquation} = createEquationBuilder({
-        numOfNodes: 3
-      });
-      stamp(5).ohms.between(0, 2);
-      stamp(5).ohms.between(1, 2);
+      const {stampResistor, getEquation} = createEquationBuilder({ numOfNodes: 3 });
+      stampResistor(5, 0, 2);
+      stampResistor(5, 1, 2);
       expect(getEquation().nodalAdmittances()).to.eql([[1 / 5, -1 / 5],
                                                        [-1 / 5, 2 / 5]]);
     });
+
     it('should throw an exception if resistance is zero', function() {
-      const {stamp} = createEquationBuilder({
-        numOfNodes: 3
-      });
+      const {stampResistor} = createEquationBuilder({ numOfNodes: 3 });
       expect(() => {
-        stamp(0).ohms.between(1, 2);
+        stampResistor(0, 1, 2);
       }).to.throw(/.*resistance.*/);
     });
+
     it('should not stamp a negative resistance', function() {
-      const {stamp} = createEquationBuilder({
-        numOfNodes: 3
-      });
+      const {stampResistor} = createEquationBuilder({ numOfNodes: 3 });
       expect(() => {
-        stamp(-1).ohms.between(1, 2);
-      }).to.throw(/.*conductance.*/);
+        stampResistor(-1, 1, 2);
+      }).to.throw(/.*resistance.*/);
     });
   });
-  describe('stamping a conductance', function() {
-    it('should stamp a conductance', function() {
-      const {stamp, getEquation} = createEquationBuilder({
-        numOfNodes: 3
-      });
-      stamp(5).siemens.between(1, 2);
-      expect(getEquation().nodalAdmittances()).to.eql([[5, -5], [-5, 5]]);
-      expect(getEquation().inputs()).to.eql(blankThreeNodeEquation.inputs());
-    });
-    it('should be additive', function() {
-      const {stamp, getEquation} = createEquationBuilder({
-        numOfNodes: 3
-      });
-      stamp(5).siemens.between(0, 2);
-      stamp(5).siemens.between(1, 2);
-      expect(getEquation().nodalAdmittances()).to.eql([[5, -5], [-5, 10]]);
-    });
-    it('should not stamp a negative conductance', function() {
-      const {stamp} = createEquationBuilder({numOfNodes: 2});
-      expect(() => {
-        stamp(-1).siemens.between(0, 1);
-      }).to.throw(/.*conductance.*/);
-    });
-  });
+
   describe('stamping a voltage source', function() {
     it('should stamp a voltage into the input vector', function() {
-      const {stamp, getEquation} = createEquationBuilder({
+      const {stampVoltageSource, getEquation} = createEquationBuilder({
         numOfNodes: 3,
         numOfVSources: 1
       });
-      stamp(5).volts.from(1).to(2);
+      stampVoltageSource(5, 1, 2);
       expect(getEquation().inputs()).to.eql([[0],
                                              [0],
                                              [5]]);
     });
+
     it('should stamp into the augmented part of the nodal admittance matrix', function() {
-      const {stamp, getEquation} = createEquationBuilder({
+      const {stampVoltageSource, getEquation} = createEquationBuilder({
         numOfNodes: 3,
         numOfVSources: 1
       });
-      stamp(5).volts.from(1).to(2);
+      stampVoltageSource(5, 1, 2);
       expect(getEquation().nodalAdmittances()).to.eql([[0, 0, 1],
                                                        [0, 0, -1],
                                                        [1, -1, 0]]);
     });
+
     it('should not stamp more than the specified number of voltage sources', function() {
-      const stamp = createEquationBuilder({
+      const {stampVoltageSource} = createEquationBuilder({
         numOfNodes: 3,
         numOfVSources: 1
-      }).stamp;
-      stamp(5).volts.from(0).to(1);
+      });
+      stampVoltageSource(5, 0, 1);
       expect(() => {
-        stamp(5).volts.from(0).to(1);
+        stampVoltageSource(5, 0, 1);
       }).to.throw(/.*Number of voltage sources stamped.*/);
     });
   });
+
   describe('stamping a current source', function() {
     it('should stamp a current source', function() {
-      const {stamp, getEquation} = createEquationBuilder({
+      const {stampCurrentSource, getEquation} = createEquationBuilder({
         numOfNodes: 3
       });
-      stamp(5).amps.from(1).to(2);
-      expect(getEquation().inputs()).to.eql([[-5],
-                                             [5]]);
-      expect(getEquation().nodalAdmittances()).to.eql(blankThreeNodeEquation.nodalAdmittances());
+      stampCurrentSource(5, 1, 2);
+      const {nodalAdmittances, inputs} = getEquation();
+      expect(inputs()).to.eql([[-5],
+                               [5]]);
+      expect(nodalAdmittances()).to.eql(blankThreeNodeEquation.nodalAdmittances());
     });
   });
+
   describe('controlled sources', function() {
     describe('stamping a current controlled current source', function() {
       it('should stamp a CCCS', function() {
-        const {stamp, getEquation} = createEquationBuilder({
+        const {stampCCCS, getEquation} = createEquationBuilder({
           numOfNodes: 4,
           numOfVSources: 1
         });
-        stamp.a.gain.of(10).multiplying.a.current.from(1).to(2).controlling.a.currentSource.from(2).to(3);
+        stampCCCS(10, 1, 2, 2, 3);
         expect(getEquation().nodalAdmittances()).to.eql([[0, 0, 0, 1],
                                                          [0, 0, 0, 9],
                                                          [0, 0, 0, -10],
                                                          [1, -1, 0, 0]]);
       });
     });
+
     describe('stamping a current controlled voltage source', function() {
       it('should stamp a CCVS', function() {
-        const {stamp, getEquation} = createEquationBuilder({
+        const {stampCCVS, getEquation} = createEquationBuilder({
           numOfNodes: 4,
           numOfVSources: 2
         });
-        stamp.a.gain.of(10).multiplying.a.current.from(1).to(2).controlling.a.voltageSource.from(2).to(3);
+        stampCCVS(10, 1, 2, 2, 3);
         expect(getEquation().nodalAdmittances()).to.eql([[0, 0, 0, 1, 0],
                                                          [0, 0, 0, -1, 1],
                                                          [0, 0, 0, 0, -1],
@@ -186,28 +160,26 @@ describe('Stamping:', function() {
                                                          [0, 1, -1, -10, 0]]);
       });
     });
+
     describe('stamping a voltage controlled current source', function() {
       it('should stamp a VCCS', function() {
-        const {stamp, getEquation} = createEquationBuilder({
+        const {stampVCCS, getEquation} = createEquationBuilder({
           numOfNodes: 4
         });
-        stamp.a.gain.of(10)
-          .multiplying.a.voltage.from(1).to(2)
-          .controlling.a.currentSource.from(2).to(3);
+        stampVCCS(10, 1, 2, 2, 3);
         expect(getEquation().nodalAdmittances()).to.eql([[0, 0, 0],
                                                          [10, -10, 0],
                                                          [-10, 10, 0]]);
       });
     });
+
     describe('stamping a voltage controlled voltage source', function() {
       it('should stamp a VCVS', function() {
-        const {stamp, getEquation} = createEquationBuilder({
+        const {stampVCVS, getEquation} = createEquationBuilder({
           numOfNodes: 4,
           numOfVSources: 1
         });
-        stamp.a.gain.of(10)
-          .multiplying.a.voltage.from(1).to(2)
-          .controlling.a.voltageSource.from(2).to(3);
+        stampVCVS(10, 1, 2, 2, 3);
         expect(getEquation().nodalAdmittances()).to.eql([[0, 0, 0, 0],
                                                          [0, 0, 0, 1],
                                                          [0, 0, 0, -1],
@@ -219,22 +191,23 @@ describe('Stamping:', function() {
 
 describe('solve', function() {
   it('should solve a simple circuit with no voltage sources', function() {
-    const {stamp, solve} = createEquationBuilder({
+    const {stampResistor, stampCurrentSource, solve} = createEquationBuilder({
       numOfNodes: 2
     });
-    stamp(1).amps.from(0).to(1);
-    stamp(100).ohms.between(1, 0);
+    stampCurrentSource(1, 0, 1);
+    stampResistor(100, 1, 0);
     const solution = solve();
     expect(solution()).to.eql([[100]]);
   });
+
   it('should solve a simple circuit with a voltage source', function() {
-    const {stamp, solve} = createEquationBuilder({
+    const {stampResistor, stampVoltageSource, stampCurrentSource, solve} = createEquationBuilder({
       numOfNodes: 3,
       numOfVSources: 1
     });
-    stamp(1).amps.from(0).to(1);
-    stamp(0).volts.from(1).to(2);
-    stamp(100).ohms.between(2, 0);
+    stampCurrentSource(1, 0, 1);
+    stampVoltageSource(0, 1, 2);
+    stampResistor(100, 2, 0);
     const solution = solve();
     expect(solution()).to.eql([[100], [100], [1]]);
   });
--- src/equation_builder.js
+++ src/equation_builder.js
@@ -12,12 +12,16 @@ const createBlankEquation = (size) => {
 };
 
 export default function({numOfNodes, numOfVSources = 0}) {
-  expect(numOfNodes, 'Number of nodes').to.be.at.least(MIN_NUM_OF_NODES);
-  expect(numOfVSources, 'Number of voltage sources').to.be.at.least(0);
-  let numOfVoltageSourcesStamped = 0;
+  expect(numOfNodes, 'Number of nodes')
+    .to.be.at.least(MIN_NUM_OF_NODES);
+  expect(numOfVSources, 'Number of voltage sources')
+    .to.be.at.least(0);
+
   const size = numOfNodes + numOfVSources - 1,
-        {nodalAdmittances, inputs} = createBlankEquation(size),
-        stampNodalAdmittanceMatrix = (row, col, x) => {
+        {nodalAdmittances, inputs} = createBlankEquation(size);
+  let numOfVoltageSourcesStamped = 0;
+
+  const stampNodalAdmittanceMatrix = (row, col, x) => {
           if (row !== 0 && col !== 0) {
             row--;
             col--;
@@ -30,145 +34,92 @@ export default function({numOfNodes, numOfVSources = 0}) {
             return inputs(set(row, 0).plusEquals(x));
           }
         },
-        stampConductance = (conductance) => {
-          return function(node1, node2) {
-            expect(conductance, 'conductance').to.be.at.least(0);
-            stampNodalAdmittanceMatrix(node1, node1, conductance);
-            stampNodalAdmittanceMatrix(node2, node2, conductance);
-            stampNodalAdmittanceMatrix(node1, node2, -conductance);
-            return stampNodalAdmittanceMatrix(node2, node1, -conductance);
-          };
-        },
-        stampResistance = (resistance) => {
-          return function(node1, node2) {
-            expect(resistance, 'resistance').to.not.equal(0);
-            const conductance = 1 / resistance;
-            return stampConductance(conductance)(node1, node2);
-          };
-        },
-        stampVoltageSource = (voltage) => {
-          return function(fromNode, toNode) {
-            expect(numOfVoltageSourcesStamped, 'Number of voltage sources stamped').to.be.lessThan(numOfVSources);
-            const vIndex = numOfNodes + numOfVoltageSourcesStamped;
-            numOfVoltageSourcesStamped++;
-            stampNodalAdmittanceMatrix(vIndex, fromNode, 1);
-            stampNodalAdmittanceMatrix(vIndex, toNode, -1);
-            stampNodalAdmittanceMatrix(fromNode, vIndex, 1);
-            stampNodalAdmittanceMatrix(toNode, vIndex, -1);
-            stampInputVector(vIndex, voltage);
-            return vIndex;
-          };
-        },
-        stampCurrentSource = (current) => {
-          return function(fromNode, toNode) {
-            stampInputVector(fromNode, -current);
-            return stampInputVector(toNode, current);
-          };
-        },
-        stampControlledSource = (gain) => {
-          return {
-            CC: function(fromControlNode, toControlNode) {
-              const vIndexControl = stampVoltageSource(0)(fromControlNode, toControlNode);
-              return {
-                CS: function(fromSourceNode, toSourceNode) {
-                  stampNodalAdmittanceMatrix(fromSourceNode, vIndexControl, gain);
-                  return stampNodalAdmittanceMatrix(toSourceNode, vIndexControl, -gain);
-                },
-                VS: function(fromSourceNode, toSourceNode) {
-                  const vIndexSource = stampVoltageSource(0)(fromSourceNode, toSourceNode);
-                  return stampNodalAdmittanceMatrix(vIndexSource, vIndexControl, -gain);
-                }
-              };
-            },
-            VC: function(fromControlNode, toControlNode) {
-              return {
-                CS: function(fromSourceNode, toSourceNode) {
-                  stampNodalAdmittanceMatrix(fromSourceNode, fromControlNode, gain);
-                  stampNodalAdmittanceMatrix(fromSourceNode, toControlNode, -gain);
-                  stampNodalAdmittanceMatrix(toSourceNode, fromControlNode, -gain);
-                  return stampNodalAdmittanceMatrix(toSourceNode, toControlNode, gain);
-                },
-                VS: function(fromSourceNode, toSourceNode) {
-                  const vIndexSource = stampVoltageSource(0)(fromSourceNode, toSourceNode);
-                  stampNodalAdmittanceMatrix(vIndexSource, fromControlNode, -gain);
-                  return stampNodalAdmittanceMatrix(vIndexSource, toControlNode, gain);
-                }
-              };
-            }
-          };
-        },
-        validateNodes = (stampFunction) => {
-          return function(fromNode, toNode) {
-            expect(fromNode, 'from node').to.be.at.least(0).and.lessThan(numOfNodes);
-            expect(toNode, 'to node').to.be.at.least(0).and.lessThan(numOfNodes);
-            return stampFunction(fromNode, toNode);
-          };
-        },
-        directional = (functionUsingNodes) => {
-          return {
-            from: function(fromNode) {
-              return {
-                to: function(toNode) {
-                  return validateNodes(functionUsingNodes)(fromNode, toNode);
-                }
-              };
-            }
-          };
-        },
-        nonDirectional = (functionUsingNodes) => {
-          return {
-            between: function(fromNode, toNode) {
-              return validateNodes(functionUsingNodes)(fromNode, toNode);
-            }
-          };
-        },
-        stampGain = (gain) => {
-          const {CC, VC} = stampControlledSource(gain);
-          const extendApi = (controllingType) => {
-            return function(fromNode, toNode) {
-              const {CS, VS} = controllingType(fromNode, toNode);
-              return {
-                controlling: {
-                  a: {
-                    currentSource: directional(CS),
-                    voltageSource: directional(VS)
-                  }
-                }
-              };
-            };
-          };
-          return {
-            multiplying: {
-              a: {
-                current: directional(extendApi(CC)),
-                voltage: directional(extendApi(VC))
-              }
-            }
-          };
-        },
-        stamp = (value) => {
-          return {
-            ohms: nonDirectional(stampResistance(value)),
-            siemens: nonDirectional(stampConductance(value)),
-            volts: directional(stampVoltageSource(value)),
-            amps: directional(stampCurrentSource(value))
-          };
+        stampConductance = (conductance, node1, node2) => {
+          stampNodalAdmittanceMatrix(node1, node1, conductance);
+          stampNodalAdmittanceMatrix(node2, node2, conductance);
+          stampNodalAdmittanceMatrix(node1, node2, -conductance);
+          return stampNodalAdmittanceMatrix(node2, node1, -conductance);
         };
 
-  stamp.a = {
-    gain: {
-      of: stampGain
-    }
-  };
+  const stampResistor = (resistance, node1, node2) => {
+          expect(resistance, 'resistance')
+            .to.be.above(0);
+          const conductance = 1 / resistance;
+          stampConductance(conductance, node1, node2);
+        },
+
+        stampVoltageSource = (voltage, fromNode, toNode) => {
+          expect(numOfVoltageSourcesStamped, 'Number of voltage sources stamped')
+            .to.be.lessThan(numOfVSources);
+          const vIndex = numOfNodes + numOfVoltageSourcesStamped;
+          numOfVoltageSourcesStamped++;
+          stampNodalAdmittanceMatrix(vIndex, fromNode, 1);
+          stampNodalAdmittanceMatrix(vIndex, toNode, -1);
+          stampNodalAdmittanceMatrix(fromNode, vIndex, 1);
+          stampNodalAdmittanceMatrix(toNode, vIndex, -1);
+          stampInputVector(vIndex, voltage);
+          return vIndex;
+        },
+
+        stampCurrentSource = (current, fromNode, toNode) => {
+          stampInputVector(fromNode, -current);
+          return stampInputVector(toNode, current);
+        },
+
+        stampCCCS = (
+            gain,
+            fromControlNode, toControlNode,
+            fromSourceNode, toSourceNode
+          ) => {
+          const vIndexControl = stampVoltageSource(0, fromControlNode, toControlNode);
+          stampNodalAdmittanceMatrix(fromSourceNode, vIndexControl, gain);
+          stampNodalAdmittanceMatrix(toSourceNode, vIndexControl, -gain);
+        },
+
+        stampCCVS = (
+            gain,
+            fromControlNode, toControlNode,
+            fromSourceNode, toSourceNode
+          ) => {
+          const vIndexControl = stampVoltageSource(0, fromControlNode, toControlNode);
+          const vIndexSource = stampVoltageSource(0, fromSourceNode, toSourceNode);
+          stampNodalAdmittanceMatrix(vIndexSource, vIndexControl, -gain);
+        },
+
+        stampVCCS = (
+            gain,
+            fromControlNode, toControlNode,
+            fromSourceNode, toSourceNode
+          ) => {
+          stampNodalAdmittanceMatrix(fromSourceNode, fromControlNode, gain);
+          stampNodalAdmittanceMatrix(fromSourceNode, toControlNode, -gain);
+          stampNodalAdmittanceMatrix(toSourceNode, fromControlNode, -gain);
+          stampNodalAdmittanceMatrix(toSourceNode, toControlNode, gain);
+        },
+
+        stampVCVS = (
+            gain,
+            fromControlNode, toControlNode,
+            fromSourceNode, toSourceNode
+          ) => {
+          const vIndexSource = stampVoltageSource(0, fromSourceNode, toSourceNode);
+          stampNodalAdmittanceMatrix(vIndexSource, fromControlNode, -gain);
+          stampNodalAdmittanceMatrix(vIndexSource, toControlNode, gain);
+        };
+
   return {
-    stamp: stamp,
-    getEquation: function() {
-      return {
-        nodalAdmittances: nodalAdmittances,
-        inputs: inputs
-      };
+    stampResistor,
+    stampVoltageSource,
+    stampCurrentSource,
+    stampCCCS,
+    stampCCVS,
+    stampVCCS,
+    stampVCVS,
+
+    getEquation: () => {
+      return {nodalAdmittances, inputs};
     },
-    solve: function() {
+    solve: () => {
       return solve(nodalAdmittances, inputs);
     }
   };
